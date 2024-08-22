4b8fa07b12cb4f1d3abfa3241a89737ca6653b3a
yara <54339485+yarstn@users.noreply.github.com>
2024-08-22 14:39:56 +0300

    Add files via upload

---
+++ src/main/java/com/example/school/Repository/CourseRepo.java
@@ -0,0 +1,9 @@
+package com.example.school.Repository;
+
+import com.example.school.Model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  CourseRepo extends JpaRepository<Course, Integer> {
+    Course findByid(Integer id);
+
+}
